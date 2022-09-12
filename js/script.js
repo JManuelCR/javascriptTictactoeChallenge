b8412980a003a6dfed2550e8376544795dd9b326
Cintia Ruiz <cintiaruizv@gmail.com>
2022-09-12 12:05:30 -0500

    Funcionalidad básica

--- js/script.js
+++ js/script.js
@@ -1,66 +1,69 @@
 let board = document.querySelectorAll(".board button");
 let isActive = false;
-let turnCounter = 1;
-let playerTurn = 0;
-const winner = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+let counter = 0;
+
+const winner = '012-345-678-036-147-258-048-246' 
+let winner2 = '012-345-678-036-147-258-048-246' 
 
 
-console.log(winner);
+console.log(winner2);
+
+document.getElementById('reset').addEventListener('click', (event) => {
+  event.preventDefault();
+  reset();
+})
 
 for (let i = 0; i < board.length; i++) {
   board[i].addEventListener('click', (event) => {
     event.preventDefault();
     
-    console.log(`Boton: ${board[i].id}`);
-    
     let jugadorActual = actualPlayer();
     if(jugadorActual == 1){  
-        board[i].innerText = 'X';
-        board[i].disabled = true;
-        console.log(winner[0]);
+      movement(i,jugadorActual,'X')
     } else if(jugadorActual == 2) { 
-        board[i].innerText = 'O';
-        board[i].disabled = true;
+        movement(i,jugadorActual,'O')
     } else {
         console.log('No funcionó');
     }
-    console.log(playerTurn);
-    mensaje = `Click en el boton: ${i}`
-    console.log(mensaje);
+  
     counter += 1;
-    console.log(counter);
     
   })
 }
 
-function actualPlayer ( )  {
-  if ( turnCounter%2 !=  0 ){
-    playerTurn = 1;
-  }else {
-    playerTurn = 2;
+function movement(i,jugadorActual,marca){
+  board[i].innerText = marca;
+  board[i].disabled = true;
+  board[i].style.color = 'white';
+  winner2 = winner2.replaceAll(i+'',marca)
+  if(winner2.indexOf(marca+marca+marca) > -1){
+    console.log(jugadorActual+' wins!!!')
+    disabledButtons(true);
   }
-  turnCounter += 1;
-
-  return playerTurn;
 }
 
-let winerCheck = () => {
-  let 
-//
-  const win = winner[i];
-  const a = board[win[0]];
-  const b = board[win[1]];
-  const c = board[win[2]];
+function disabledButtons(disabled){
+  for (let i = 0; i < board.length; i++) {
+    board[i].disabled = disabled;
+  }
 }
-//a ===
+
+function actualPlayer ( )  {
+  if ( counter%2 ==  0 ){
+    return 1;
+  }else {
+    return 2;
+  }
+}
+
+function reset(){
+  for (let i = 0; i < board.length; i++) {
+    board[i].innerText = '';
+    board[i].disabled = false;
+  }
+  winner2 = winner;
+  counter = 0;
+}
+
         
 
